6630aa866c61f649d8f8bb4d96964aada5f128a9
Diego Luis <contato.diegoluisdasilva@gmail.com>
2021-10-23 20:14:38 -0300

    Criando Assets

--- src/data/categories.ts
+++
@@ -1,7 +0,0 @@
-import { Category } from "../types/Category";
-
-export const categories: Category = {
-  food: { title: "Alimentação", color: "#2b429e", expense: true },
-  rent: { title: "Aluguel", color: "#9e612b", expense: true },
-  salary: { title: "Salário", color: "#3d7849", expense: false },
-};
